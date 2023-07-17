99a37086b1b052dc3a3d0af5fe220d6290193b08
Hoso <hossam.alasklany@gmail.com>
2023-07-17 14:05:24 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
 import "./App.css";
-
+import About from "./components/About";
+import Header from "./components/Header";
+import Home from "./components/Home";
+import Skills from "./components/Skills";
+import Services from "./components/Services";
+import Qualification from "./components/Qualification";
+import Testimonials from "./components/Testi/Testimonials";
+import MyWork from "./components/work/MyWork";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer/Footer";
+import ScrollUp from "./components/Scroll-to-up/ScrollUp";
 function App() {
   return (
     <>
-      <h1>hellow hoso</h1>
+      <Header />
+      <main>
+        <Home />
+        <About />
+        <Skills />
+        <Services />
+        <Qualification />
+        <MyWork />
+        <Testimonials />
+        <Contact />
+      </main>
+      <Footer />
+      <ScrollUp />
     </>
   );
 }
